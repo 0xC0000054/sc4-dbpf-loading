10d9fd04cc22165b8b583be6e9dc06c79550d1fb
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-02-07 04:04:47 -0700

    Add a MultiPackedFile class for loose .SC4* files
    The LooseSC4MultiPackedFile class reads the TGI values from a hash map,
    which is much faster than the per-file linear search SC4 used
    previously.
    The downside it that is uses more memory.
    
    LooseSC4MultiPackedFile is not derived from MultiPackedFile to allow
    the code to be debugged without breakpoints also getting hit for DAT
    files.
    Additionally, the required search order for GetResourceKeyList and
    ConsolidateDatabaseRecords is different between the two classes.
    
    MultiPackedFile uses a first in first out search order (front to back),
    same as SC4's cGZPersistDBSegmentMultiPackedFiles class.
    
    LooseSC4MultiPackedFile uses a last in first out search order
    (back to front), same as SC4's cGZPersistResourceManager class.

---
+++ src/LooseSC4PluginMultiPackedFile.h
@@ -0,0 +1,123 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of sc4-dbpf-loading, a DLL Plugin for SimCity 4 that
+// optimizes the DBPF loading.
+//
+// Copyright (c) 2024, 2025 Nicholas Hayes
+//
+// This file is licensed under terms of the MIT License.
+// See LICENSE.txt for more information.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "cIGZPersistDBSegment.h"
+#include "cIGZPersistDBSegmentMultiPackedFiles.h"
+#include "cRZBaseString.h"
+#include "cRZBaseUnknown.h"
+#include "PersistResourceKeyBoostHash.h"
+#include "PersistResourceKeyHash.h"
+#include "boost/unordered/unordered_flat_map.hpp"
+#include <vector>
+#include <Windows.h>
+
+class cIGZCOM;
+class cIGZPersistResourceManager;
+
+// A cIGZPersistDBSegmentMultiPackedFiles implementation for loose .SC4* files (.SC4Desc, .SC4Lot, and .SC4Model).
+// This class replaces the game's liner search code with a per-TGI lookup.
+//
+// The search order used for GetResourceKeyList and ConsolidateDatabaseRecords is different from the
+// .DAT implementation in MultiPackedFile due to the behavior of the code it is replacing.
+//
+// MultiPackedFile uses a first in first out search order (front to back), same as SC4's
+// cGZPersistDBSegmentMultiPackedFiles class.
+//
+// LooseSC4MultiPackedFile uses a last in first out search order (back to front) search order, same as SC4's
+// cGZPersistResourceManager class.
+class LooseSC4MultiPackedFile final : public cRZBaseUnkown, public cIGZPersistDBSegment, public cIGZPersistDBSegmentMultiPackedFiles
+{
+public:
+
+	LooseSC4MultiPackedFile();
+
+	~LooseSC4MultiPackedFile();
+
+	bool QueryInterface(uint32_t riid, void** ppvObj) override;
+
+	uint32_t AddRef() override;
+
+	uint32_t Release() override;
+
+	// cIGZPersistDBSegment
+
+	bool Init() override;
+	bool Shutdown() override;
+
+	bool Open(bool openRead, bool openWrite) override;
+	bool IsOpen() const override;
+	bool Close() override;
+	bool Flush() override;
+
+	void GetPath(cIGZString& path) const override;
+	bool SetPath(cIGZString const& path) override;
+
+	bool Lock() override;
+	bool Unlock() override;
+
+	uint32_t GetSegmentID() const override;
+	bool SetSegmentID(uint32_t const& segmentID) override;
+
+	uint32_t GetRecordCount(cIGZPersistResourceKeyFilter* filter) override;
+
+	uint32_t GetResourceKeyList(cIGZPersistResourceKeyList* list, cIGZPersistResourceKeyFilter* filter) override;
+	bool GetResourceKeyList(cIGZPersistResourceKeyList& list) override;
+
+	bool TestForRecord(cGZPersistResourceKey const& key) override;
+	uint32_t GetRecordSize(cGZPersistResourceKey const& key) override;
+	bool OpenRecord(cGZPersistResourceKey const& key, cIGZPersistDBRecord** record, uint32_t fileAccessMode) override;
+	bool CreateNewRecord(cGZPersistResourceKey const& key, cIGZPersistDBRecord** record) override;
+
+	bool CloseRecord(cIGZPersistDBRecord* record) override;
+	bool CloseRecord(cIGZPersistDBRecord** record) override;
+
+	bool AbortRecord(cIGZPersistDBRecord* record) override;
+	bool AbortRecord(cIGZPersistDBRecord** record) override;
+
+	bool DeleteRecord(cGZPersistResourceKey const& key) override;
+	uint32_t ReadRecord(cGZPersistResourceKey const& key, void* buffer, uint32_t& recordSize) override;
+	bool WriteRecord(cGZPersistResourceKey const& key, void* buffer, uint32_t recordSize) override;
+
+	bool Init(uint32_t segmentID, cIGZString const& path, bool unknown2) override;
+
+	// cIGZPersistDBSegmentMultiPackedFiles
+
+	void SetPathFilter(cIGZString const&) override;
+
+	int32_t ConsolidateDatabaseRecords(cIGZPersistDBSegment*, cIGZPersistResourceKeyFilter*) override;
+	int32_t ConsolidateDatabaseRecords(cIGZString const&, cIGZPersistResourceKeyFilter*) override;
+
+	bool FindDBSegment(cGZPersistResourceKey const&, cIGZPersistDBSegment**) override;
+	uint32_t GetSegmentCount() override;
+	cIGZPersistDBSegment* GetSegmentByIndex(uint32_t) override;
+
+	void AddedResource(cGZPersistResourceKey const&, cIGZPersistDBSegment*) override;
+	void RemovedResource(cGZPersistResourceKey const&, cIGZPersistDBSegment*) override;
+
+private:
+
+	bool Open(const std::vector<cRZBaseString>& datFiles);
+
+	bool SetupGZPersistDBSegment(
+		cIGZString const& path,
+		cIGZCOM* const pCOM);
+
+	uint32_t segmentID;
+	cRZBaseString folderPath;
+	bool initialized;
+	bool isOpen;
+	CRITICAL_SECTION criticalSection;
+	boost::unordered::unordered_flat_map<const cGZPersistResourceKey, cIGZPersistDBSegment*> tgiMap;
+	std::vector<cIGZPersistDBSegment*> segments;
+};
+
--- src/LooseSC4PluginScanPatch.cpp
+++ src/LooseSC4PluginScanPatch.cpp
@@ -11,167 +11,93 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "LooseSC4PluginScanPatch.h"
+#include "LooseSC4PluginMultiPackedFile.h"
 #include "Logger.h"
 #include "Patcher.h"
-#include "SC4DirectoryEnumerator.h"
-#include "cIGZString.h"
+#include "cIGZCOM.h"
+#include "cIGZDBSegmentPackedFile.h"
+#include "cIGZPersistDBSegment.h"
+#include "cIGZPersistResourceManager.h"
+#include "cISC4App.h"
+#include "cRZAutoRefCount.h"
+#include "cRZBaseString.h"
+#include "cRZCOMDllDirector.h"
+#include "GZServPtrs.h"
 
 namespace
 {
-	static void* RZStringVtable = reinterpret_cast<void*>(0xA80810);
-
-	class cRZString
+	void LoadSC4FilesFromDirectory(const cIGZString& rootDir)
 	{
-	public:
-		cIGZString* AsIGZString()
-		{
-			return reinterpret_cast<cIGZString*>(this);
-		}
-
-		void* vtable;
-		intptr_t stdStringField1;
-		intptr_t stdStringField2;
-		intptr_t stdStringField3;
-		uint32_t refCount;
-	};
-
-	typedef cRZString* (__thiscall* cRZString_ctor_cIGZString)(cRZString* pThis, cIGZString const& str);
-
-	static const cRZString_ctor_cIGZString RZString_ctor_cIGZString = reinterpret_cast<cRZString_ctor_cIGZString>(0x40AE00);
-
-	enum cRZString_dtor_Options : uint8_t
-	{
-		RZStrDtorOptionClassIsStackVariable = 0,
-		RZStrDtorOptionClassIsHeapAllocated = 1,
-		RZStrDtorOptionUnknown = 2,
-	};
-
-	typedef cRZString* (__thiscall* cRZString_dtor)(cRZString* pThis, cRZString_dtor_Options options);
-
-	static const cRZString_dtor RZString_dtor = reinterpret_cast<cRZString_dtor>(0x44BF10);
-
-	typedef void(__thiscall* std_list_cRZString_insert)(void* pList, cRZString const& str);
-
-	static const std_list_cRZString_insert ListInsert = reinterpret_cast<std_list_cRZString_insert>(0x4580E0);
-
-	static bool __fastcall ScanDirectoryForLooseSC4Plugins(
-		void* rzDirectoryThisPtr,
-		void* edxUnused,
-		cIGZString* rootDir,
-		void* rzStringList,
-		bool includeCurrentDirFiles)
-	{
-		// This method replaces the cRZDirectory::ReadDirectoryFileEntriesIntoStringListRecursive call
-		// that the game uses to scan for the loose .SC4* plugins.
-		//
-		// Unlike the original method, we check the file extension as part of the file scanning code.
-		// This ensures that we only perform the UTF-16 to UTF-8 string conversion for items with a .SC4*
-		// file extension, and it also reduces the amount of memory that needs to be allocated for the output
-		// lists.
-
 		bool result = false;
 
-		try
+		cRZAutoRefCount<cIGZPersistDBSegment> looseSC4MultiPackedFile(
+			new LooseSC4MultiPackedFile(),
+			cRZAutoRefCount<cIGZPersistDBSegment>::kAddRef);
+
+		if (looseSC4MultiPackedFile->Init())
 		{
-			std::vector<cRZBaseString> sc4Files;
-
-			SC4DirectoryEnumerator::ScanDirectoryForLooseSC4FilesRecursive(*rootDir, sc4Files);
-
-			for (const cRZBaseString& path : sc4Files)
+			if (looseSC4MultiPackedFile->SetPath(rootDir))
 			{
-				// The cRZString class is a reimplementation of the game's internal cRZString class.
-				// We need to do this for compatibility with the game's std::list<cRZString>::insert method
-				// that we call to insert the item into this method's output list.
-				//
-				// We allocate our class on the stack and call the game's cRZString constructor and destructor.
+				if (looseSC4MultiPackedFile->Open(true, false))
+				{
+					cIGZPersistResourceManagerPtr resMan;
 
-				cRZString rzStr{};
-				RZString_ctor_cIGZString(&rzStr, path);
-
-#ifdef _DEBUG
-				const char* const path2 = rzStr.AsIGZString()->ToChar();
-#endif // _DEBUG
-
-
-				// Insert the copied string into the output list.
-				ListInsert(rzStringList, rzStr);
-
-				RZString_dtor(&rzStr, RZStrDtorOptionClassIsStackVariable);
+					if (resMan)
+					{
+						result = resMan->RegisterDBSegment(*looseSC4MultiPackedFile);
+					}
+				}
 			}
 
-			result = true;
+			if (!result)
+			{
+				looseSC4MultiPackedFile->Shutdown();
+			}
 		}
-		catch (const std::exception& e)
-		{
-			Logger::GetInstance().WriteLine(LogLevel::Error, e.what());
-			result = false;
-		}
-
-		return result;
 	}
 
-	static int32_t __cdecl HookedIsDatabaseFile(cIGZString const& path, bool scanEntireFile)
-	{
-#ifdef _DEBUG
-		//PrintLineToDebugOutput(path.ToChar());
-#endif // _DEBUG
+	static constexpr uintptr_t SC4InstallationPluginDirectoryScan_Inject = 0x457a07;
+	static constexpr uintptr_t SC4InstallationPluginDirectoryScan_Continue = 0x457B50;
 
-		// SC4 checks every file in its plugins folders that does not have a .dat file extension to
-		// see if it is a DBPF file.
-		// Because our replacement directory scanning code already filters out any files that
-		// don't have a .SC4* file extension, we just return true.
-		//
-		// The method treats -1 as false, and any other value as true.
-
-		return 1;
-	}
-
-	static constexpr uintptr_t SC4InstallationPluginDirectoryScan_Inject = 0x457A2F;
-	static constexpr uintptr_t SC4InstallationPluginSkipFileExtensionCheck_Inject = 0x457A4B;
-	static constexpr uintptr_t SC4InstallationPluginSkipFileExtensionCheck_Continue = 0x457A6A;
-	static constexpr uintptr_t SC4InstallationPluginIsDatabaseFile_Inject = 0x457A9F;
-
-	static void NAKED_FUN SC4InstallationPluginSkipFileExtensionCheck()
+	static void NAKED_FUN LoadSC4FilesForInstallationPluginsHook()
 	{
 		__asm
 		{
-			// Cleanup the stack parameters for the cRZFile::GetPathExtension call we are replacing.
-			add esp, 8;
-			// Skip the remaining file extension check code.
-			push SC4InstallationPluginSkipFileExtensionCheck_Continue;
-			ret;
+			// We do not need to preserve any registers.
+			lea eax, [esp + 0x3c]
+			push eax // directory path
+			call LoadSC4FilesFromDirectory // (cdecl)
+			add esp, 4
+			mov eax, SC4InstallationPluginDirectoryScan_Continue
+			jmp eax
 		}
 	}
 
-	static constexpr uintptr_t UserPluginDirectoryScan_Inject = 0x457CAE;
-	static constexpr uintptr_t UserPluginSkipFileExtensionCheck_Inject = 0x457CCA;
-	static constexpr uintptr_t UserPluginSkipFileExtensionCheck_Continue = 0x457CE9;
-	static constexpr uintptr_t UserPluginIsDatabaseFile_Inject = 0x457D1E;
+	static constexpr uintptr_t UserPluginDirectoryScan_Inject = 0x457C86;
+	static constexpr uintptr_t UserPluginDirectoryScan_Continue = 0x457DCF;
 
-	static void NAKED_FUN UserPluginSkipFileExtensionCheck()
+	static void NAKED_FUN LoadSC4FilesForUserPluginsHook()
 	{
 		__asm
 		{
-			// Cleanup the stack parameters for the cRZFile::GetPathExtension call we are replacing.
-			add esp, 8;
-			// Skip the remaining file extension check code.
-			push UserPluginSkipFileExtensionCheck_Continue;
-			ret;
+			// We do not need to preserve any registers.
+			lea eax, [esp + 0x3c]
+			push eax // directory path
+			call LoadSC4FilesFromDirectory // (cdecl)
+			add esp, 4
+			mov eax, UserPluginDirectoryScan_Continue
+			jmp eax
 		}
 	}
 
 	void InstallSC4InstallationPluginsDirScanPatch()
 	{
-		Patcher::InstallCallHook(SC4InstallationPluginDirectoryScan_Inject, &ScanDirectoryForLooseSC4Plugins);
-		Patcher::InstallHook(SC4InstallationPluginSkipFileExtensionCheck_Inject, &SC4InstallationPluginSkipFileExtensionCheck);
-		Patcher::InstallCallHook(SC4InstallationPluginIsDatabaseFile_Inject, &HookedIsDatabaseFile);
+		Patcher::InstallHook(SC4InstallationPluginDirectoryScan_Inject, &LoadSC4FilesForInstallationPluginsHook);
 	}
 
 	void InstallUserDirScanPatch()
 	{
-		Patcher::InstallCallHook(UserPluginDirectoryScan_Inject, &ScanDirectoryForLooseSC4Plugins);
-		Patcher::InstallHook(UserPluginSkipFileExtensionCheck_Inject, &UserPluginSkipFileExtensionCheck);
-		Patcher::InstallCallHook(UserPluginIsDatabaseFile_Inject, &HookedIsDatabaseFile);
+		Patcher::InstallHook(UserPluginDirectoryScan_Inject, &LoadSC4FilesForUserPluginsHook);
 	}
 }
 
